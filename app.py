7832720c8e85bb71fb599db0ec5fe11729feaca1
humaiarad <d.humaiara@gmail.com>
2026-09-15 21:38:09 -0400

    server wokring

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to our Shop"
+    return jsonify({"message": "Welcome to our shop!"})
 
 
 @app.route("/products", methods=["GET"])
